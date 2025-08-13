61347067447dcfa3fa67eaa570ce795c7e2b4357
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2025-08-13 19:42:42 +0500

    add button component

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
+  @Input() label: string = 'Label';
+  @Output() clicked = new EventEmitter<void>();
 
+  handleClick() {
+    this.clicked.emit()
+  }
 }
