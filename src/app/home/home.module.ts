ccd16948b9350f40920fd57b7fecb433b619437c
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2025-08-13 21:46:27 +0500

    add hero section

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeroSectionComponent
   ],
   imports: [
     CommonModule,
