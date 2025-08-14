c120c033752e9d7b2f86d7ea4fae113a243cfa3e
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2025-08-14 16:20:00 +0500

    create footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ButtonComponent } from './button/button.component';
+import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [ButtonComponent, NavbarComponent],
+  declarations: [ButtonComponent, NavbarComponent, FooterComponent],
   imports: [CommonModule, FontAwesomeModule, RouterLink, RouterLinkActive],
-  exports: [ButtonComponent, NavbarComponent]
+  exports: [ButtonComponent, NavbarComponent, FooterComponent]
 })
 export class SharedModule {}
