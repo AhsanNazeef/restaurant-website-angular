1b7e6fbff60573b0d07349ccb7dd246a3296fcc7
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2025-08-21 16:37:17 +0500

    create contact page hero section

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,15 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ContactComponent } from './contact.component';
-
-
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
-  declarations: [
-    ContactComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ContactComponent, HeroComponent],
+  imports: [CommonModule, RouterLink]
 })
-export class ContactModule { }
+export class ContactModule {}
